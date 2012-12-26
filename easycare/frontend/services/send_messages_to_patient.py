afd56b098324a673283f196519557ef979b48033
Chaiyawut Sookplang <chaiyawut.so@gmail.com>
2012-12-26 14:32:19 +0700

    test sms

--- easycare/frontend/services/send_messages_to_patient.py
+++ easycare/frontend/services/send_messages_to_patient.py
@@ -12,7 +12,7 @@ def send_messages_to_patient(msg_type, contact_number, contact_email, reply_mess
 
 		Key = 'dnLjIPco1OCeOfnGFjI5dgLj8vvrhW'
 		Mobile = contact_number
-		Message = u'สวัสดีครับ'
+		Message = 'สวัสดีครับ'.encode('utf-8')
 
 		client = SoapClient(wsdl="http://cronos.kmutt.ac.th/smswebservice/send.asmx?WSDL",trace=False)
 		response = client.SendSMS(Key, Mobile, Message)
