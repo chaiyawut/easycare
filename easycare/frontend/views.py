99167ade16cd7be29b3d4c07c31e7bfd281e9015
Chaiyawut Sookplang <chaiyawut.so@gmail.com>
2013-03-13 17:40:39 +0700

    added remind message to patient

--- easycare/frontend/views.py
+++ easycare/frontend/views.py
@@ -47,7 +47,7 @@ def patient_remind(request, patient_id):
 	html_messages = render_to_string('email/remind.html')
 	sent = send_messages_to_patient(patient.confirm_by, patient.contact_number, patient.email, reply_messages, html_messages)
 	if sent:
-		messages.success(request, "ข้อความ..." + reply_messages + "...ถูกส่งไปเตือนคุณ " + patient.fullname.encode('utf-8') + " แล้ว", extra_tags='alert alert-success')
+		messages.success(request, "ข้อความถูกส่งไปเตือนคุณ " + patient.fullname.encode('utf-8') + " แล้ว", extra_tags='alert alert-success')
 	else:
 		messages.success(request, "ระบบส่งข้อความผิดพลาด คนไข้จะไม่ได้รับข้อความ", extra_tags='alert alert-error')
 	return redirect(success_url)
